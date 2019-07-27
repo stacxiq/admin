6097f660b480258fc817b411ed589efd9b0ea794
AliAlsed <alialsed1996@yahoo.com>
2019-07-27 09:43:45 +0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCLc3vtRULhmV5nNvW6ofXw8Le6NPC1g8o',
+    authDomain: 'wechat-2b611.firebaseapp.com',
+    databaseURL: 'https://wechat-2b611.firebaseio.com',
+    projectId: 'wechat-2b611',
+    storageBucket: 'wechat-2b611.appspot.com',
+    messagingSenderId: '459435392019',
+    appId: '1:459435392019:web:3b5cfa12f996f1af'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
